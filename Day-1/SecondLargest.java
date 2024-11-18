c7f7d62b7f106e78cad3aacf43abc84c7784d685
MedidhiManeesha <medidhimaneesha@gmail.com>
2024-11-18 23:20:33 +0530

    2 sums done

--- Day-1/SecondLargest.java
+++ Day-1/SecondLargest.java
@@ -1,5 +1,15 @@
 public class SecondLargest{
     public static void main(String[] args) {
-        System.out.println("Started");
+        int first = 0;
+        int second = 0;
+        int[] arr = {3,12,34,53,5};
+        for(int i=0; i<arr.length; i++){
+            if(first < arr[i]){
+                second = first;
+                first = arr[i];
+            }else if(arr[i] > second && arr[i] < first){
+                second = arr[i];
+            }
+        }
     }
 }
